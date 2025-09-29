6896f2d0386df3f698dfab4fe4856fc79a73888c
yuNuki2 <umrsuth_1373@yahoo.co.jp>
2025-09-29 21:04:48 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
-  console.log(window.location);
+  useEffect(() => {
+    console.log(window.location);
+  }, []);
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
