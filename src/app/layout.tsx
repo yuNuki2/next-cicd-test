2ed6943708ba20df6f6911a3e67c8f4cb0872ed7
yuNuki2 <umrsuth_1373@yahoo.co.jp>
2025-09-29 21:01:43 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  console.log(process.env);
+
   return (
     <html lang="en">
       <body
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import Image from "next/image";
 
 export default function Home() {
+  console.log(window.location);
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
